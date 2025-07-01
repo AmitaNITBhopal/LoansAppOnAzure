38cb4a8d164171b04777c7613c9ea93874a3d521
AmitaNITBhopal <amita.nitbhopal@gmail.com>
2025-07-01 18:07:32 +0200

    made changes in pom.xml for azure

--- Omega/Back/backend/src/test/java/com/omegadummy/backend/BackendApplicationTests.java
+++ Omega/Back/backend/src/test/java/com/omegadummy/backend/BackendApplicationTests.java
@@ -8,6 +8,16 @@ class BackendApplicationTests {
 
 	@Test
 	void contextLoads() {
+		System.out.println("\n Inside Test1\n");
 	}
 
+	@Test
+	void contextLoads2() {
+		System.out.println("\n Inside Test2\n");
+	}
+	
+	@Test
+	void contextLoads1() {
+		System.out.println("\n Inside Test3\n");
+	}
 }
